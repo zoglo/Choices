2a3b184a29f58384bd5b8617578d40ebb19aff33
Xon <635541+Xon@users.noreply.github.com>
2024-08-06 00:13:14 +0800

    Rebuild public/types

--- public/types/src/scripts/lib/PreEscapedString.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class PreEscapedString {
-    readonly s: string;
-    constructor(unescapedString: string);
-    toString(): string;
-}
-//# sourceMappingURL=PreEscapedString.d.ts.map
--- public/types/src/scripts/lib/UntrustedString.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class UntrustedString {
-    readonly s: string;
-    readonly raw: string;
-    constructor(unescapedString: string);
-    toString(): string;
-}
-//# sourceMappingURL=UntrustedString.d.ts.map
