1dde6e09b2f7bc94780e3f42d53bb3588e06923d
Xon <635541+Xon@users.noreply.github.com>
2024-08-07 22:55:21 +0800

    Mock window.matchMedia for tests

---
+++ test/setupFiles/window-matchMedia.ts
@@ -0,0 +1,17 @@
+import { beforeAll } from 'vitest';
+
+beforeAll(() => {
+  Object.defineProperty(globalThis.window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
+});
--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: ['./test/setupFiles/window-matchMedia.ts'],
   },
   esbuild: {
     target: 'es2017',
