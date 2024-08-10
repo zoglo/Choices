5c9bca640b01a20fa0e83445d0d38e5411090618
Xon <635541+Xon@users.noreply.github.com>
2024-08-10 14:55:27 +0800

    Ensure consts are actually const

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,6 +1,6 @@
-export const TEXT_TYPE: HTMLInputElement['type'] = 'text';
-export const SELECT_ONE_TYPE: HTMLSelectElement['type'] = 'select-one';
+export const TEXT_TYPE: HTMLInputElement['type'] = 'text' as const;
+export const SELECT_ONE_TYPE: HTMLSelectElement['type'] = 'select-one' as const;
 export const SELECT_MULTIPLE_TYPE: HTMLSelectElement['type'] =
-  'select-multiple';
+  'select-multiple' as const;
 
-export const SCROLLING_SPEED = 4;
+export const SCROLLING_SPEED = 4 as const;
