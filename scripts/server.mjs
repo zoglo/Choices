545d880e2c5b5173aad16352e624296ff66dc703
Xon <635541+Xon@users.noreply.github.com>
2024-08-17 21:07:32 +0800

    Fix wrong key for caching playwright data & reduce e2e test log spam

--- scripts/server.mjs
+++ scripts/server.mjs
@@ -2,15 +2,16 @@ import dev from 'rollup-plugin-dev';
 
 export default function server() {
   const WATCH_HOST = process.env.WATCH_HOST;
-  const WATCH_PORT = process.env.WATCH_PORT || 3001;
 
   if (!WATCH_HOST) {
     return void 0;
   }
+  const WATCH_PORT = process.env.WATCH_PORT || 3001;
 
   return dev({
     dirs: ['public'],
     host: WATCH_HOST,
     port: WATCH_PORT,
+    silent: process.env.CI || false
   });
 };
