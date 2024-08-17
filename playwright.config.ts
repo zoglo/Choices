e72284fd0dc4f54c921c5a354a46039215465e6f
Xon <635541+Xon@users.noreply.github.com>
2024-08-17 21:12:36 +0800

    Adjust timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,9 @@ const config: PlaywrightTestConfig = {
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'dot',//process.env.CI ? 'dot' : 'list',
   timeout: 2000,
+  expect : {
+    timeout: 500,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
