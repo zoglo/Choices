cbb0d6523a7b39b0856f8b5f6d7451f7dbe2f063
Xon <635541+Xon@users.noreply.github.com>
2024-08-21 22:55:32 +0800

    Update the URL used in e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -79,7 +79,7 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:3001',
+    url: 'http://localhost:3001',
     reuseExistingServer: !process.env.CI,
   },
 };
