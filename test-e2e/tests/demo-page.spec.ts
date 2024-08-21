01f4ea370fb4a7ddbe1a44f5846c99cd15478a87
Xon <635541+Xon@users.noreply.github.com>
2024-08-22 01:22:44 +0800

    Use a HAR files to mock the external API to avoid token API usages from breaking the test

--- test-e2e/tests/demo-page.spec.ts
+++ test-e2e/tests/demo-page.spec.ts
@@ -11,26 +11,20 @@ describe(`Choices`, () => {
   const testUrl = '/index.html';
   const testId = 'reset-simple';
   test.setTimeout(30000);
-  test('screenshot', async ({ page, bundle }) => {
-    // const artifactsPath = 'screenshot';
-    // const browser = 'firefail';
-    // const snapshotName = `${process.platform}-${browser}.png`;
 
+  test('screenshot', async ({ page, bundle }) => {
     const suite = new TestSuit(page, bundle, testUrl, testId);
+
+    await page.routeFromHAR('./test-e2e/hars/discogs.har', {
+      url: 'https://api.discogs.com/**',
+      update: false, // https://playwright.dev/docs/mock#replaying-from-har
+    });
 
     await suite.start();
 
     await page.click('label[for="choices-single-custom-templates"]');
     await suite.input.press('ArrowDown');
     await suite.input.press('ArrowDown');
-
-    /*
-    mkdirSync(artifactsPath, { recursive: true });
-    await page.screenshot({
-      path: path.join(artifactsPath, snapshotName),
-      fullPage: true,
-    });
-*/
 
     await expect(page).toHaveScreenshot({
       fullPage: true,
