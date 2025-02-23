b77846d8656d529a80cbddcdbd9d2e57bdc5fd0c
midzer <midzer@gmail.com>
2025-02-23 01:32:51 +0100

    add SearchByKMP

--- public/types/src/scripts/interfaces/build-flags.d.ts
+++ public/types/src/scripts/interfaces/build-flags.d.ts
@@ -1,9 +1,11 @@
 export declare const canUseDom: boolean;
 export declare const searchFuse: string | undefined;
+export declare const searchKMP: string | undefined;
 /**
  * These are not directly used, as an exported object (even as const) will prevent tree-shake away code paths
  */
 export declare const BuildFlags: {
     readonly searchFuse: string | undefined;
+    readonly searchKMP: string | undefined;
     readonly canUseDom: boolean;
 };
--- src/scripts/search/index.ts
+++ src/scripts/search/index.ts
@@ -2,11 +2,15 @@ import { Options } from '../interfaces';
 import { Searcher } from '../interfaces/search';
 import { SearchByPrefixFilter } from './prefix-filter';
 import { SearchByFuse } from './fuse';
-import { searchFuse } from '../interfaces/build-flags';
+import { SearchByKMP } from './kmp';
+import { searchFuse, searchKMP } from '../interfaces/build-flags';
 
 export function getSearcher<T extends object>(config: Options): Searcher<T> {
   if (searchFuse) {
     return new SearchByFuse<T>(config);
+  }
+  if (searchKMP) {
+    return new SearchByKMP<T>(config);
   }
 
   return new SearchByPrefixFilter<T>(config);
